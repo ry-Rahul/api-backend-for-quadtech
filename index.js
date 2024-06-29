7890941524f88ff0eeb14e8945ca1a9a88f37990
ry-Rahul <ryrahul12345@gmail.com>
2024-06-29 20:17:32 +0530

    connected

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ import dotenv from "dotenv";
 
 dotenv.config();
 // Connect to MongoDB
-connectDB(process.env.MONGO_URI);
+connectDB('mongodb+srv://ryrahul12345:Eds4wwzUQAT5lUQm@cluster0.38hmvpw.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0');
 // Fetch and store data into MongoDB
 fetchData(); 
 
@@ -24,6 +24,7 @@ const __dirname = path.resolve();
 const public_path = path.join(__dirname, "public");
 app.use(express.static(public_path));
 
+app.use('/api', router);
 
 app.get("/", (req, res) => {
     res.sendFile(path.join(public_path, "index.html"));
@@ -33,7 +34,6 @@ app.get("/about", (req, res) => {
     res.status(200).json({ message: "About page" });
 });
 
-app.use('/api', router);
 
 app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`.bgGreen);
